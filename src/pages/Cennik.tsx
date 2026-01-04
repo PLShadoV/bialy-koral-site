b237adeef3ec176c076cb4f79d19825f7bbe9478
PLShadoV <plshadov@gmail.com>
2026-01-04 12:09:53 +0100

    3456732764

--- src/pages/Cennik.tsx
+++ src/pages/Cennik.tsx
@@ -17,13 +17,14 @@ const Cennik = () => {
 
   const pricing = [
     { period: "11.04–31.05", price: "280 zł" },
+    { period: "Wielkanoc 01.04-07.04", price: "350 zł" },
     { period: "Majówka 29.04 - 03.05", price: "350 zł" },
     { period: "01.06–14.06", price: "320 zł" },
-    { period: "14.06–28.06", price: "350 zł" },
-    { period: "28.06–05.07", price: "450 zł" },
-    { period: "05.07–12.07", price: "470 zł" },
-    { period: "12.07–16.08", price: "550 zł" },
-    { period: "16.08–31.08", price: "450 zł" },
+    { period: "15.06–28.06", price: "350 zł" },
+    { period: "29.06–05.07", price: "450 zł" },
+    { period: "06.07–12.07", price: "470 zł" },
+    { period: "13.07–16.08", price: "550 zł" },
+    { period: "17.08–31.08", price: "450 zł" },
     { period: "01.09–19.10", price: "270 zł" },
   ];
 
