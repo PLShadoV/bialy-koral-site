9a797454683304ca4e253c99e75fb5c841eaac64
PLShadoV <plshadov@gmail.com>
2026-06-18 20:29:59 +0200

    45848

--- src/AppHashRouter.tsx
+++ src/AppHashRouter.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HashRouter, Routes, Route } from "react-router-dom";
+import { LanguageProvider } from "./i18n/LanguageContext";
 import Home from "./pages/Home";
 import Galeria from "./pages/Galeria";
 import Cennik from "./pages/Cennik";
@@ -14,24 +15,25 @@ const queryClient = new QueryClient();
 
 const AppHashRouter = () => {
   console.log("AppHashRouter component rendering");
-  
+
   return (
     <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <HashRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/galeria" element={<Galeria />} />
-            <Route path="/cennik" element={<Cennik />} />
-            <Route path="/kontakt" element={<Kontakt />} />
-            <Route path="/rezerwacja" element={<Rezerwacja />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </HashRouter>
-      </TooltipProvider>
+      <LanguageProvider>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          <HashRouter>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/galeria" element={<Galeria />} />
+              <Route path="/cennik" element={<Cennik />} />
+              <Route path="/kontakt" element={<Kontakt />} />
+              <Route path="/rezerwacja" element={<Rezerwacja />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </HashRouter>
+        </TooltipProvider>
+      </LanguageProvider>
     </QueryClientProvider>
   );
 };
